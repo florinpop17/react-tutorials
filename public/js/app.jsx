384a9e3db34d5465f63bcce17cc4c56bc919a590
Florin Pop <popflorin1705@yahoo.com>
2016-12-13 21:11:24 +0200

    completed simple react app

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -4,14 +4,34 @@ var GreeterMessage = require('../components/GreeterMessage');
 var GreeterForm = require('../components/GreeterForm');
 
 var Greeter = React.createClass({
-    render : function () {
+    getDefaultProps: function() {
+        return {
+            name: 'React',
+            message: 'This is the default message!'
+        };
+    },
+    getInitialState: function() {
+        return {
+            name: this.props.name,
+            message: this.props.message
+        };
+    },
+    handleNewData: function(updates) {
+        this.setState(updates);
+    },
+    render: function(){
+        var name = this.state.name;
+        var message = this.state.message;
+        
         return (
             <div>
-                <GreeterMessage />
-                <GreeterForm />
+                <GreeterMessage name={name} message={message} />
+                <GreeterForm onNewData={this.handleNewData} />
             </div>
         );
     }
 });
 
-ReactDOM.render(<Greeter />, document.getElementById("react-container"));
+var myName = "Florin";
+
+ReactDOM.render(<Greeter name={myName} />, document.getElementById("react-container"));
