a9cfdb7ae6ff66cc1c5ebd9afada54264fcbfe69
Florin Pop <popflorin1705@yahoo.com>
2016-12-13 19:33:26 +0200

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,9 @@ module.exports = {
                 loader: 'babel-loader',
                 query: {
                     presets: ['react', 'es2015']
-                }
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
             }
         ]
     }
