a8b1d28468b17ccbb291e590bfec1d72ecdbc12a
Florin Pop <popflorin1705@yahoo.com>
2016-12-13 19:40:10 +0200

    added GreeterMessage react component

---
+++ public/components/GreeterMessage.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+    
+var GreeterMessage = React.createClass({
+   render: function (){
+       var name = this.props.name;
+       var message = this.props.message;
+       
+       return {
+           <div>
+               <h1>Hello {name}</h1>
+               <p>{message}</p>
+           </div>
+       };
+   } 
+});
+
+module.exports = GreeterMessage;
